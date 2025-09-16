d4296095999f7c912c0359083b578468bfbdfd8e
SAMERkab <samerkab2001@gmail.com>
2025-09-16 18:45:47 +0200

    Set pwsh encoding to UTF-8 everywhere.
    In pwsh, the default encoding that is used when communicating with
    external programs is 'OEM United States'.

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,9 @@
-$env:EDITOR = "nvim"
-$env:SHELL = "pwsh"
-$env:MYSHELL = "pwsh"
-$env:NOTES = "~\Desktop\notes\"
-$env:VAULT = "~\Desktop\files\"
+[Console]::InputEncoding = [Console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
 $env:MY_MODULE = "$(Split-Path $PROFILE)\Modules\MyModule\MyModule.psm1"
 
 Set-Alias less "C:\Program Files\Git\usr\bin\less.exe"
 Set-Alias edit $env:EDITOR
-Set-Alias lua luart
 
 Invoke-Expression ((chezmoi completion powershell) -join "`n")
 
