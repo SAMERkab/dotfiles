35311fec5facafe46c40ec2545f67752e592b9c8
SAMERkab <samerkab2001@gmail.com>
2025-09-20 18:54:48 +0200

    Enable the equivalent of Ctrl-X Ctrl-E in pwsh (opens command in $EDITOR)

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,15 @@
+# Import-Module -Name Terminal-Icons
+# Import-Module PSFzf
+
 [Console]::InputEncoding = [Console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 $env:MY_MODULE = "$(Split-Path $PROFILE)\Modules\MyModule\MyModule.psm1"
 
 Set-Alias less "C:\Program Files\Git\usr\bin\less.exe"
 Set-Alias edit $env:EDITOR
+
+Set-PSReadLineOption -EditMode Vi
+Set-PSReadLineKeyHandler -Chord Alt+e -Function ViEditVisually
 
 Invoke-Expression ((chezmoi completion powershell) -join "`n")
 
